d664d65ba55e497ce4f17d4aa03d8676f9d66120
kimjeonghyun <invent819@naver.com>
2022-11-05 11:20:22 +0900

    first commit

---
+++ EscapeRoom/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace EscapeRoom
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.itemBox = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnLaserSound = new System.Windows.Forms.Button();
+            this.btnBackGroundSound = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.MXP = new AxWMPLib.AxWindowsMediaPlayer();
+            this.mediaPlayer = new AxWMPLib.AxWindowsMediaPlayer();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timerSliding = new System.Windows.Forms.Timer(this.components);
+            this.checkBoxHide = new System.Windows.Forms.CheckBox();
+            this.itemBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.MXP)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mediaPlayer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // itemBox
+            // 
+            this.itemBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.itemBox.Controls.Add(this.checkBoxHide);
+            this.itemBox.Controls.Add(this.label1);
+            this.itemBox.Dock = System.Windows.Forms.DockStyle.Right;
+            this.itemBox.Location = new System.Drawing.Point(1216, 0);
+            this.itemBox.Name = "itemBox";
+            this.itemBox.Size = new System.Drawing.Size(200, 948);
+            this.itemBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(70, 22);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 36);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "아이템";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(554, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 28);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(554, 82);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 28);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(307, 90);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 18);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnLaserSound
+            // 
+            this.btnLaserSound.Location = new System.Drawing.Point(67, 162);
+            this.btnLaserSound.Name = "btnLaserSound";
+            this.btnLaserSound.Size = new System.Drawing.Size(135, 85);
+            this.btnLaserSound.TabIndex = 6;
+            this.btnLaserSound.Text = "Play Laser Sound";
+            this.btnLaserSound.UseVisualStyleBackColor = true;
+            this.btnLaserSound.Click += new System.EventHandler(this.PlayLaserSound);
+            // 
+            // btnBackGroundSound
+            // 
+            this.btnBackGroundSound.Location = new System.Drawing.Point(360, 154);
+            this.btnBackGroundSound.Name = "btnBackGroundSound";
+            this.btnBackGroundSound.Size = new System.Drawing.Size(160, 93);
+            this.btnBackGroundSound.TabIndex = 7;
+            this.btnBackGroundSound.Text = "Play Background Music - Sound Player";
+            this.btnBackGroundSound.UseVisualStyleBackColor = true;
+            this.btnBackGroundSound.Click += new System.EventHandler(this.PlayBackgroundSoundPlayer);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(185, 253);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(176, 114);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Play Background Music - Media Player";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.PlayMediaPlayer);
+            // 
+            // MXP
+            // 
+            this.MXP.Enabled = true;
+            this.MXP.Location = new System.Drawing.Point(399, 320);
+            this.MXP.Name = "MXP";
+            this.MXP.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("MXP.OcxState")));
+            this.MXP.Size = new System.Drawing.Size(291, 47);
+            this.MXP.TabIndex = 9;
+            // 
+            // mediaPlayer
+            // 
+            this.mediaPlayer.Enabled = true;
+            this.mediaPlayer.Location = new System.Drawing.Point(660, 90);
+            this.mediaPlayer.Name = "mediaPlayer";
+            this.mediaPlayer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("mediaPlayer.OcxState")));
+            this.mediaPlayer.Size = new System.Drawing.Size(870, 410);
+            this.mediaPlayer.TabIndex = 10;
+            this.mediaPlayer.Enter += new System.EventHandler(this.mediaPlayer_Enter);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::EscapeRoom.Properties.Resources._11;
+            this.pictureBox3.Location = new System.Drawing.Point(12, 386);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(223, 291);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 11;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Image = global::EscapeRoom.Properties.Resources._2;
+            this.pictureBox2.Location = new System.Drawing.Point(753, 26);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(206, 302);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::EscapeRoom.Properties.Resources._1;
+            this.pictureBox1.Location = new System.Drawing.Point(256, 464);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(511, 394);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // timerSliding
+            // 
+            this.timerSliding.Interval = 10;
+            this.timerSliding.Tick += new System.EventHandler(this.timerSliding_Tick);
+            // 
+            // checkBoxHide
+            // 
+            this.checkBoxHide.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.checkBoxHide.ForeColor = System.Drawing.Color.White;
+            this.checkBoxHide.Location = new System.Drawing.Point(0, 898);
+            this.checkBoxHide.Name = "checkBoxHide";
+            this.checkBoxHide.Size = new System.Drawing.Size(200, 50);
+            this.checkBoxHide.TabIndex = 13;
+            this.checkBoxHide.Text = "<";
+            this.checkBoxHide.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1416, 948);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.mediaPlayer);
+            this.Controls.Add(this.MXP);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnBackGroundSound);
+            this.Controls.Add(this.btnLaserSound);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.itemBox);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.itemBox.ResumeLayout(false);
+            this.itemBox.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.MXP)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mediaPlayer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel itemBox;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnLaserSound;
+        private System.Windows.Forms.Button btnBackGroundSound;
+        private System.Windows.Forms.Button button3;
+        private AxWMPLib.AxWindowsMediaPlayer MXP;
+        private AxWMPLib.AxWindowsMediaPlayer mediaPlayer;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Timer timerSliding;
+        private System.Windows.Forms.CheckBox checkBoxHide;
+    }
+}
+
---
+++ EscapeRoom/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.Drawing.Imaging;
+using System.Threading;
+
+
+
+
+namespace EscapeRoom
+{
+    public partial class Form1 : Form
+    {
+        //슬라이딩 메뉴의 최대, 최소 폭 크기
+        const int MAX_SLIDING_WIDTH = 200;
+        const int MIN_SLIDING_WIDTH = 50;
+        //슬라이딩 메뉴가 보이는/접히는 속도 조절
+        const int STEP_SLIDING = 10;
+        //최초 슬라이딩 메뉴 크기
+        int _posSliding = 200;
+
+
+
+        SoundPlayer backgroundMusic;
+        SoundPlayer laserSound;
+        bool bgPlaying = false;
+
+
+
+
+        int opacity = 240;
+        Image original = null;
+        PictureBox sourceImage = new PictureBox();
+        bool[] hasItem = new bool[10];
+        
+        public class Item
+        {
+            public static List<PictureBox> items = new List<PictureBox>();
+            
+        }
+        public Form1()
+        {
+            Item item = new Item();
+            InitializeComponent(); this.MouseMove += new MouseEventHandler(Mouse_Move);
+
+            mediaPlayer.URL = @"C:\Users\inven\Videos\2022-05-24 10-33-45.mkv";
+
+            MXP.Ctlcontrols.play();
+            MXP.URL = @"C:\Users\inven\OneDrive\바탕 화면\EscapeRoom\EscapeRoom\Resources\backgroundSound.mp3";
+            MXP.settings.playCount = 9999; // repeating the music when it ends
+            MXP.Ctlcontrols.stop();
+            MXP.Visible = false;
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Item.items.Add(new PictureBox()); 
+            pictureBox2.Visible = true;
+            if (bgPlaying == true)
+            {
+                backgroundMusic.Stop();
+                bgPlaying = false;
+            }
+           
+            MXP.Ctlcontrols.play(); 
+
+        }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Point local = this.PointToClient(Cursor.Position);
+            e.Graphics.DrawEllipse(Pens.Red, local.X - 25, local.Y - 25, 20, 20);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+            pictureBox2.Visible = true;
+            playSimpleSound();
+
+            sourceImage.ImageLocation = "../../Resources/1.jpg";
+            sourceImage.SizeMode = PictureBoxSizeMode.StretchImage;
+            sourceImage.Size = new Size(120, 120);
+            sourceImage.Location = new Point(30, 50);
+            sourceImage.Cursor = Cursors.Hand;
+            sourceImage.Click += ItemClick;
+            itemBox.Controls.Add(sourceImage);
+        }
+        
+
+        public void ItemClick(object sender, EventArgs e)
+        {
+            sourceImage.Size = new Size(140, 140);
+            sourceImage.Location = new Point(20, 40);
+            sourceImage.ImageLocation = "../../Resources/2.jpg";
+            SoundPlayer simpleSound = new SoundPlayer(@"c:\Windows\Media\chimes.wav");
+            simpleSound.Play();
+            
+        }
+        public void playSimpleSound()
+        {
+            SoundPlayer simpleSound = new SoundPlayer(@"C:\Users\inven\Music\backgroundSound.wav");
+            simpleSound.Play();
+        }
+
+
+        private void Mouse_Move(object sender, EventArgs e)
+        {
+
+            //X-ray
+            int x = Cursor.Position.X;
+            //Y-ray
+            int y = Cursor.Position.Y;
+            this.textBox1.Text = x.ToString();
+            this.textBox2.Text = y.ToString(); 
+            mediaPlayer.StatusChange += mediaExit;
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            if (!hasItem[0])
+            {
+                label2.Text = "아무일도 없습니다.";
+                if (original == null) original = (Bitmap)pictureBox1.Image.Clone();
+                timer1.Start();
+               
+            }
+
+        }
+
+
+
+
+
+
+
+
+        static Bitmap SetAlpha(Bitmap bmpIn, int alpha)
+        {
+            Bitmap bmpOut = new Bitmap(bmpIn.Width, bmpIn.Height);
+            float a = alpha / 255f;
+            Rectangle r = new Rectangle(0, 0, bmpIn.Width, bmpIn.Height);
+
+                float[][] matrixItems = {
+            new float[] {1, 0, 0, 0, 0},
+            new float[] {0, 1, 0, 0, 0},
+            new float[] {0, 0, 1, 0, 0},
+            new float[] {0, 0, 0, a, 0},
+            new float[] {0, 0, 0, 0, 1}};
+
+            ColorMatrix colorMatrix = new ColorMatrix(matrixItems);
+
+            ImageAttributes imageAtt = new ImageAttributes();
+            imageAtt.SetColorMatrix(colorMatrix, ColorMatrixFlag.Default, ColorAdjustType.Bitmap);
+
+            using (Graphics g = Graphics.FromImage(bmpOut))
+                g.DrawImage(bmpIn, r, r.X, r.Y, r.Width, r.Height, GraphicsUnit.Pixel, imageAtt);
+
+            return bmpOut;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            if (opacity > 0.0)
+            {
+
+                opacity -= 30;
+                pictureBox1.Image = SetAlpha((Bitmap)original, opacity);
+                
+            }
+            else
+            {
+                
+                timer1.Stop(); 
+                
+            }
+        }
+
+        private void PlayLaserSound(object sender, EventArgs e)
+        {
+            laserSound = new SoundPlayer(@"C:\Users\inven\OneDrive\바탕 화면\EscapeRoom\EscapeRoom\Resources\laserSound.wav");
+            laserSound.Play();
+        }
+
+        private void PlayBackgroundSoundPlayer(object sender, EventArgs e)
+        {
+            backgroundMusic = new SoundPlayer(@"C:\Users\inven\OneDrive\바탕 화면\EscapeRoom\EscapeRoom\Resources\flashlight-click.wav");
+            backgroundMusic.PlayLooping();
+            //MXP.Ctlcontrols.stop(); // stop the media player
+           // bgPlaying = true; // set playing to true
+        }
+
+        private void PlayMediaPlayer(object sender, EventArgs e)
+        {
+            if (bgPlaying == true)
+            {
+                backgroundMusic.Stop();
+                bgPlaying = false;
+            }
+            
+            MXP.Ctlcontrols.play();
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mediaPlayer_Enter(object sender, EventArgs e)
+        {
+            mediaPlayer.fullScreen = true;
+            
+            
+        }
+        public void mediaExit(object sender, EventArgs e)
+        {
+            mediaPlayer.Visible=false;
+        }
+
+        private void timerSliding_Tick(object sender, EventArgs e)
+        {
+            if (checkBoxHide.Checked == true)
+            {
+                //슬라이딩 메뉴를 숨기는 동작
+                _posSliding -= STEP_SLIDING;
+                if (_posSliding <= MIN_SLIDING_WIDTH)
+                    timerSliding.Stop();
+            }
+            else
+            {
+                //슬라이딩 메뉴를 보이는 동작
+                _posSliding += STEP_SLIDING;
+                if (_posSliding >= MAX_SLIDING_WIDTH)
+                    timerSliding.Stop();
+            }
+
+            itemBox.Width = _posSliding;
+        }
+    }
+}
